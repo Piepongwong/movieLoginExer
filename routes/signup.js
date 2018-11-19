f6d5a5b4d7448b31e120c282bed4e89d80b83809
anakellyc <anakcampos@gmail.com>
2018-11-19 22:10:26 +0100

    Update

--- routes/signup.js
+++ routes/signup.js
@@ -9,27 +9,34 @@ app.get('/', function(req, res) {
 })
 
 app.post("/", function(req, res) {
-  
-  //
-  bcrypt.hash(req.body.password, 5, function(err, encryptedPassword) {
-    if (err) res.send("ERROR")
+  var user = new User(req.body);
+  User.find({email: req.body.email})
+  .then((result)=>{
+    if (result.length == 0) {
+      bcrypt.hash(req.body.password, 5, function(err, encryptedPassword) {
+        if (err) res.send("ERROR")
+        else {
+          User.create({
+            username: req.body.username,
+            email: req.body.email,
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            password: encryptedPassword
+          })
+          .then(()=>{
+            res.cookie("loggedIn", "true", {signed: true})
+            res.render("yourspace", {loggedIn: true})
+          })
+          .catch((err)=>{
+            res.end("ERROR", err)
+          })
+        }
+      });
+    }
     else {
-    User.create({
-      username: req.body.username,
-      email: req.body.email,
-      firstname: req.body.firstname,
-      lastname: req.body.lastname,
-      password: encryptedPassword
-    })
-    .then(()=>{
-      res.cookie("loggedIn", "true", {signed: true})
-      res.render("yourspace", {loggedIn: true})
-    })
-    .catch((err)=>{
-      res.end("ERROR", err)
-    })
-  }
-  });
+      res.render("signuperror", {loggedIn: false})
+    }
+  })
 })
 
 module.exports = app
